58853030825aa7bfd23da3bf3745335a85142593
Poligorn <133873347+Poligorn@users.noreply.github.com>
2023-10-06 11:11:29 +0300

    Add files via upload

--- Item.cs
+++ Item.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+
+    public GameObject pickupEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             Player player = collision.GetComponent<Player>();
             player.points++;
+            GameObject effect = Instantiate(pickupEffect, transform.position, transform.rotation);
+            Destroy(effect, 5);
             Destroy(this.gameObject);
         }
     }
--- Obstacle.cs
+++ Obstacle.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    public float scaleMax = 1.3f;
+    public float scaleMin = 0.7f;
+
+    public float rotationMax = 180f;
+    public float rotationMin = 0f;
+
+    public GameObject explosionPlayer;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            Player player = collision.GetComponent<Player>();
+            GameObject effect = Instantiate(explosionPlayer, player.transform.position, player.transform.rotation);
+            Destroy(effect, 5);
             Destroy(collision.gameObject);
         }
     }
+
+    private void Start()
+    {
+        randomSize();
+        randomRotation();
+    }
+
+    void randomSize()
+    {
+        float scaleFactor = Random.Range(scaleMin, scaleMax);
+        transform.localScale = (Vector2)transform.localScale * scaleFactor;
+    }
+
+
+    void randomRotation()
+    {
+        float rotationFactor = Random.Range(rotationMin, rotationMax);
+        transform.localEulerAngles = Vector3.forward * rotationFactor;
+    }
+
+
 }
